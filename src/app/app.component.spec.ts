7934cc7cd24ba1fc476e08c1b615f9e1b11f70b0
Tomas Campusano <tomascampusano.10@gmail.com>
2023-03-10 01:38:55 -0300

    Added: Tailwind

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularNew'`, () => {
+  it(`should have as title 'angularAdidasLandingPage'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularNew');
+    expect(app.title).toEqual('angularAdidasLandingPage');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angularNew app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angularAdidasLandingPage app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Adidas';
 
-  changeTitle() {
-    this.title = "changed";
-  }
 }
