2193f5bdc380a0afd12b71c1dd5b5a771b35a613
Tomas Campusano <tomascampusano.10@gmail.com>
2023-03-24 18:28:01 -0300

    Added: slider for novedades on main

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import novedadesData from '../../novedades.json'
+
+interface Novedad {
+  id: Number,
+  name: String;
+  image: String;
+  price: Number;
+  category: String;
+  extra: String;
+}
 
 @Component({
   selector: 'app-main',
@@ -6,6 +16,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent {
+
+  novedades: Novedad[] = novedadesData
 
   selector = 1
 
@@ -17,7 +29,7 @@ export class MainComponent {
   selector1() {
     this.selector = 1
   }
-  
+
   selector2() {
     this.selector = 2
   }
